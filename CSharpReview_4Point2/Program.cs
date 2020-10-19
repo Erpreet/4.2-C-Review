b64566eef303be0e229fadc1bff2b64a01e0e1e3
Erpreet <preetkaurchd15@gmail.com>
2020-10-19 10:09:06 -0600

    data types, if condition added

--- CSharpReview_4Point2/Program.cs
+++ CSharpReview_4Point2/Program.cs
@@ -6,11 +6,41 @@ namespace CSharpReview_4Point2
     {
         static void Main(string[] args)
         {
+            char aChar;
+            string aString;
+
+            bool aBoolean;
+
+
+            byte aByte;
+            short aShortInt;
+            int anInt;
+            long aLongInt;
+
+            float aFloat;
+            double aDouble;
+            decimal aDecimal;
+
             string name;
 
-            name = Console.ReadLine();
-            Console.ReadLine();
-            Console.WriteLine("Hello World!What a nice day today!");
+            Console.Write("Please enter you name");
+
+            name = Console.ReadLine().Trim();
+
+
+            if (name == "")
+            {
+                Console.WriteLine("Invalid Input: Please enter a name");
+            }
+            else
+            {
+                name = Console.ReadLine();
+                Console.ReadLine();
+                Console.WriteLine("Hello " + name + "!What a nice day today!");
+                Console.WriteLine("Hello {0}! What a nice day today!", name);
+                Console.WriteLine($"Hello {name}!What a nice day today!");
+            }
         }
+
     }
 }
