e04c8da5a70e8c5ffaeab07e9f22ab201f4c431b
Erpreet <preetkaurchd15@gmail.com>
2020-10-19 10:24:38 -0600

    program for class exercise

--- CSharpReview_4Point2/Program.cs
+++ CSharpReview_4Point2/Program.cs
@@ -1,46 +1,67 @@
 ﻿using System;
 
 namespace CSharpReview_4Point2
-{
-    class Program
     {
-        static void Main(string[] args)
+        class Program
         {
-            char aChar;
-            string aString;
-
-            bool aBoolean;
-
-
-            byte aByte;
-            short aShortInt;
-            int anInt;
-            long aLongInt;
-
-            float aFloat;
-            double aDouble;
-            decimal aDecimal;
-
-            string name;
-
-            Console.Write("Please enter you name");
-
-            name = Console.ReadLine().Trim();
-
-
-            if (name == "")
+            static void Main(string[] args)
+            // Entry point of programs in C#.
             {
-                Console.WriteLine("Invalid Input: Please enter a name");
-            }
-            else
-            {
-                name = Console.ReadLine();
-                Console.ReadLine();
-                Console.WriteLine("Hello " + name + "!What a nice day today!");
-                Console.WriteLine("Hello {0}! What a nice day today!", name);
-                Console.WriteLine($"Hello {name}!What a nice day today!");
+                // In-Class Exercise: 
+                // Make the "Admin" decision branch case-insensitive.
+
+                // Declaration of a variable.
+                char aChar; // A single character value.
+                string aString; // String (collection of characters) values.
+
+                bool aBoolean; // Boolean (true-false) values (bit).
+
+                byte aByte; // Super small integer values (~120 max).
+                short aShortInt; // Small integer values (~65000 max).
+                int anInt; // Integer values (~210000000 max).
+                long aLongInt; // BIG integer values (9223372036854775807 max).
+
+                float aFloat; // Floating point values (less precision than doubles).
+                double aDouble; // Decimal values (like a float but more precise).
+                decimal aDecimal; // Also decimal values (extremely precise).
+
+
+                string name;
+
+                // Write a prompt.
+                // Write will keep the cursor on the same line, WriteLine will not.
+                Console.Write("Please enter your name: ");
+
+                // Input from the console.
+                // Trim() will remove all leading and trailing whitespace (newlines, spaces, tabs, etc).
+                name = Console.ReadLine().Trim();
+
+                // Condition is an expression that evaluates to either true or false. 
+                // True runs the "if" side, false runs the "else" side.
+
+                // Roughly equivalent to name == "", except it also treats null as empty, and any whitespace.
+                // Whitespace should be already trimmed, but the extra layer doesn't necessarily hurt.
+                if (string.IsNullOrWhiteSpace(name))
+                {
+                    Console.WriteLine("Invalid Input: Please enter a name.");
+                }
+                else if (name == "Admin")
+                {
+                    Console.WriteLine("Administrative access granted.");
+                }
+                else
+                {
+                    // Output to the console.
+                    // Concatenation.
+                    Console.WriteLine("Hello " + name + "! What a nice day today!");
+                    // Parameterized Output.
+                    Console.WriteLine("Hello {0}! What a nice day today!", name);
+                    // String Interpolation.
+                    Console.WriteLine($"Hello {name}! What a nice day today!");
+                }
+
+
+                // Exit point of programs in C#.
             }
         }
-
     }
-}
